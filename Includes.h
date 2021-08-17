6f30654929e75cea01b647f4ad236e167779c2e9
Liam-Swain <74320380+Liam-Swain@users.noreply.github.com>
2021-08-17 13:38:43 -0500

    Delete Includes.h

--- Includes.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include "player/Player.h"
-#include "Characters/ArcheryClass.h"
-#include "Characters/Knight.h"
-#include "Characters/Wizard.h"
-#include "Characters/Druid.h"
